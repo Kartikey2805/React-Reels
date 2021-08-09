473e8014b15218976c3c8a1e9adb21494dc06827
Kartikey <kartikeygoel093@gmail.com>
2021-08-09 12:19:31 +0530

    ready for deployment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Login from './components/Login';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Signup from './components/Signup';
+import Feed from './components/Feed';
+import PrivateRoute from './privateAuth/privateRoute';
+import { AuthContextProvider } from './contexts/AuthProvider';
+import './styles/styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Switch>
+          <PrivateRoute path='/' exact component={Feed}></PrivateRoute>
+          <Route path='/login' component={Login}></Route>
+          <Route path='/signup' component={Signup}></Route>
+        </Switch>
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/secrets.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apiKey: 'AIzaSyBcIwpwKcrpLi6OTpmgi8VyLxZgbzzoWLU',
+  authDomain: 'fir-auth-1a91d.firebaseapp.com',
+  projectId: 'fir-auth-1a91d',
+  storageBucket: 'fir-auth-1a91d.appspot.com',
+  messagingSenderId: '111173530913',
+  appId: '1:111173530913:web:57f318589b7f68cb4b3a60',
+};
